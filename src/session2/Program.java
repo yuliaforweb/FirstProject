fd9341a78b3f9f4417fd2d80808f9438e7541021
yuliaforweb <108054948+yuliaforweb@users.noreply.github.com>
2022-06-28 00:56:25 +0300

    Update Program.java
    fix Student student = new Student();

--- src/session2/Program.java
+++ src/session2/Program.java
@@ -3,7 +3,7 @@ package session2;
 public class Program {
 
     public static void main(String[] args) {
-        Student studentObject = new Student();
+        Student student = new Student();
          student.setName("Yuliia Kobzeva");
         student.setJavaLevel("Basic");
         student.printDetails();
